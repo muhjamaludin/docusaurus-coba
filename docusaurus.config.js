c0495da61e418a2b95fdca99b45ab56987d7e912
Muh Jamaludin <74maludin@gmail.com>
2021-10-24 01:44:47 +0700

    fix site not loading

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://muhjamaludin.github.io',
-  baseUrl: '/',
+  baseUrl: '/docusaurus-coba/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
